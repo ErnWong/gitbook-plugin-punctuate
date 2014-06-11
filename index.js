23964dd0b8b399d4e2c034d839a1bdd05b21ec74
ErnWong <ewon521@gmail.com>
2014-06-11 17:05:07 +1200

    More debug.
    Escaped periods in regexp, and actually replaced content string.

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var dictionary = [
-    {find: /``/g, replace: "“"},    // left double quotes (do before single quote)
-    {find: /''/g, replace: "”"},    // right double quotes (do before single quote)
-    {find: /`/g,  replace: "‘"},    // left single quote
-    {find: /'/g,  replace: "’"},    // right single quote
-    {find: /.../g,  replace: "…"},  // ellipses
-    {find: /---/g,  replace: "—"},  // em dash (do before en dash)
-    {find: /--/g,  replace: "–"},   // en dash (do before hyphen)
-    {find: /-/g,  replace: "‐"}     // hyphen (do before hyphen)
+    {find: /``/g, replace: "“"},    	// left double quotes (do before single quote)
+    {find: /''/g, replace: "”"},    	// right double quotes (do before single quote)
+    {find: /`/g,  replace: "‘"},    	// left single quote
+    {find: /'/g,  replace: "’"},    	// right single quote
+    {find: /\.\.\./g,  replace: "…"},  	// ellipses
+    {find: /---/g,  replace: "—"},  	// em dash (do before en dash)
+    {find: /--/g,  replace: "–"},   	// en dash (do before hyphen)
+    {find: /-/g,  replace: "‐"}     	// hyphen (do before hyphen)
 ];
 
 module.exports = {
     hooks: {
         "page:before": function(page) {
+			var content = page.content;
             for (var i = 0, len = dictionary.length; i<len; i++) {
-                page.content.replace(dictionary[i].find, dictionary[i].replace);
+                content = content.replace(dictionary[i].find, dictionary[i].replace);
             }
+			page.content = content;
             return page;
         }
     }
