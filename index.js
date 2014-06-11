75d07790ff4c86fa82d4b9030f6e15ed590922be
ErnWong <ewon521@gmail.com>
2014-06-11 17:51:23 +1200

    Revert "Disabled hyphens"
    This reverts commit 033ab3257f0b6e539247b8147b817062c2efbf9f.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var dictionary = [
-    {find: /``/g, replace: "“"},        // left double quotes (do before single quote)
-    {find: /''/g, replace: "”"},        // right double quotes (do before single quote)
-    {find: /`/g,  replace: "‘"},        // left single quote
-    {find: /'/g,  replace: "’"},        // right single quote
+    {find: /``/g, replace: "“"},    	// left double quotes (do before single quote)
+    {find: /''/g, replace: "”"},    	// right double quotes (do before single quote)
+    {find: /`/g,  replace: "‘"},    	// left single quote
+    {find: /'/g,  replace: "’"},    	// right single quote
     {find: /\.\.\./g,  replace: "…"},  	// ellipses
-    {find: /---/g,  replace: "—"},      // em dash (do before en dash)
-    {find: /--/g,  replace: "–"}        // en dash (do before hyphen)
-    //{find: /-/g,  replace: "‐"}         // hyphen (do before hyphen)
-                                        // hyphen disabled in favour of MathJax
+    {find: /---/g,  replace: "—"},  	// em dash (do before en dash)
+    {find: /--/g,  replace: "–"},   	// en dash (do before hyphen)
+    {find: /-/g,  replace: "‐"}     	// hyphen (do before hyphen)
 ];
 
 module.exports = {
